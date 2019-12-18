d3a0cad306264310d2b69ae08e3aaf2dd72dd084
Sourav <souravdasslg95@gmail.com>
2019-12-18 20:55:07 +0530

    merge sorted array is completed

---
+++ 18 Dec/Merge Sorted Array/mergeSortedArray.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+const mergeSortedArray = (nums1, m, nums2, n) => {
+  let first = m - 1;
+  let second = n - 1;
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (second < 0) {
+      break;
+    }
+    if (nums1[first] > nums2[second]) {
+      nums1[i] = nums1[first];
+      first--;
+    } else {
+      nums1[i] = nums2[second];
+      second--;
+    }
+  }
+  return nums1
+};
+export {
+  mergeSortedArray
+}
